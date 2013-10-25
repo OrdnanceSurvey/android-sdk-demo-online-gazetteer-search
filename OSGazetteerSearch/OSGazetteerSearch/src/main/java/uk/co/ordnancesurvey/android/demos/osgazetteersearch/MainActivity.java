4b17b2b1d35296305837483ef52c0b18a4c2cf4e
Suman Chatterjee (C) <Suman.Chatterjee@ordnancesurvey.co.uk>
2013-10-25 14:01:11 +0100

    Add serachview widget in the actionbar

--- OSGazetteerSearch/OSGazetteerSearch/src/main/java/uk/co/ordnancesurvey/android/demos/osgazetteersearch/MainActivity.java
+++ OSGazetteerSearch/OSGazetteerSearch/src/main/java/uk/co/ordnancesurvey/android/demos/osgazetteersearch/MainActivity.java
@@ -1,11 +1,33 @@
 package uk.co.ordnancesurvey.android.demos.osgazetteersearch;
 
 import android.app.Activity;
+import android.app.SearchManager;
+import android.app.SearchableInfo;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.SearchView.OnQueryTextListener;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
 
 import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
 
+import uk.co.ordnancesurvey.android.maps.FailedToLoadException;
+import uk.co.ordnancesurvey.android.maps.Geocoder;
 import uk.co.ordnancesurvey.android.maps.GridPoint;
 import uk.co.ordnancesurvey.android.maps.OSMap;
 import uk.co.ordnancesurvey.android.maps.OSTileSource;
@@ -13,9 +35,10 @@ import uk.co.ordnancesurvey.android.maps.MapFragment;
 import uk.co.ordnancesurvey.android.maps.MarkerOptions;
 import uk.co.ordnancesurvey.android.maps.BitmapDescriptorFactory;
 import uk.co.ordnancesurvey.android.maps.BitmapDescriptor;
+import uk.co.ordnancesurvey.android.maps.Placemark;
 
 
-public class MainActivity extends Activity implements OSMap.OnMapClickListener{
+public class MainActivity extends Activity implements OnQueryTextListener {
 
     /**
      * This API Key is registered for this application.
@@ -23,15 +46,24 @@ public class MainActivity extends Activity implements OSMap.OnMapClickListener{
      * Define your own OS Openspace API KEY details below
      * @see http://www.ordnancesurvey.co.uk/oswebsite/web-services/os-openspace/index.html
      */
-    private final static String OS_API_KEY = "E93E8C82D5B81A4DE0430B6CA40AAB92";
+    private final static String OS_API_KEY = "E93E8C82D5BC1A4DE0430B6CA40AAB92";
 
-    private final static boolean OS_IS_PRO = true;
-
+    private final static boolean OS_IS_PRO = false;
 
     private final static String TAG = MainActivity.class.getSimpleName();
 
+
+    private static final EnumSet<Geocoder.GeocodeType> POSTCODE
+            = EnumSet.of(Geocoder.GeocodeType.OnlinePostcode);
+
+    private static final EnumSet<Geocoder.GeocodeType> PLACES
+            = EnumSet.of(Geocoder.GeocodeType.OnlineGazetteer);
+
+    private Geocoder mGeocoder;
     private OSMap mMap;
 
+    private TextView  mStatusView;
+    private SearchView mSearchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,13 +82,11 @@ public class MainActivity extends Activity implements OSMap.OnMapClickListener{
         sources.add(mMap.webTileSource(OS_API_KEY, OS_IS_PRO, null));
 
         mMap.setTileSources(sources);
-
-        // register as OnMapClickListener
-        mMap.setOnMapClickListener(this);
+        mStatusView = new TextView(this);
 
         Log.v(TAG, "onCreate complete.");
 
-    }
+}
 
 
     @Override
@@ -64,29 +94,93 @@ public class MainActivity extends Activity implements OSMap.OnMapClickListener{
 
         super.onDestroy();
 
+        if (mGeocoder != null) {
+            mGeocoder.close();
+            mGeocoder = null;
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.actions, menu);
+        mSearchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        mSearchView.setOnQueryTextListener(this);
+        return true;
     }
 
 
     @Override
-    public boolean onMapClick(GridPoint gp)
-    {
-
-        final String locationMessage = String.format("Map tapped at OS GridPoint\n{%.0f, %.0f}", gp.x, gp.y);
-
-        Log.v(TAG, locationMessage);
-
-        BitmapDescriptor icon = BitmapDescriptorFactory.defaultMarker();
-
-        mMap.addMarker(new MarkerOptions()
-                .gridPoint(gp)
-                .title("Map clicked here!")
-                .snippet(locationMessage)
-                .icon(icon));
-
-        return true;
-
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return super.onPrepareOptionsMenu(menu);
     }
 
+    @Override
+    public boolean    onOptionsItemSelected (MenuItem item) {
+        Toast.makeText(this, "Selected Item: " + item.getTitle(), Toast.LENGTH_SHORT).show();
+        return true;
+    }
+
+    // The following callbacks are called for the SearchView.OnQueryChangeListener
+    public boolean onQueryTextChange(String newText) {
+        newText = newText.isEmpty() ? "" : "Query so far: " + newText;
+     //   setContentView(mSearchText);
+        return true;
+    }
+
+    public boolean onQueryTextSubmit(String query) {
+        //Toast.makeText(this, "Searching for: " + query + "...", Toast.LENGTH_SHORT).show();
+        if (query == null || query.trim().length() == 0) {
+            return true;
+        }
+      //  performQuery(query);
+         mSearchView.clearFocus();
+        return true;
+    }
+
+    public void performQuery(String query) {
+
+        new PlaceQueryTask().execute(query);
+    }
+
+    class PlaceQueryTask extends AsyncTask<String, Void, Geocoder.Result> {
+
+        @Override
+        protected Geocoder.Result doInBackground(String... queries) {
+
+            if (queries.length == 0) {
+                return null;
+            }
+
+            String query = queries[0];
+
+            if (query == null) {
+                return null;
+            }
+
+            query = query.trim();
+
+            if (query.length() == 0) {
+                return null;
+            }
+
+            return mGeocoder.geocodeString(query, PLACES, null, 0, 15);
+        }
+
+
+        @Override
+        protected void onPostExecute(Geocoder.Result results) {
+
+            if (results == null) {
+                return;
+            }
+
+      //      mAdapter.addAll(results.getPlacemarks());
+
+       //     mAdapter.notifyDataSetChanged();
+        }
+    }
 
 
 }
